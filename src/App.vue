85228e389b0f072b2b0fcabcdb3d5dc37bb9d165
Yormi Altamiranda <yormian@gmail.com>
2023-03-06 07:19:07 -0500

    :white_check_mark: update 10 mixins

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
-	<h1>{{ text }}</h1>
-</template>
+	<div>{{ text }}</div>
+	<div>{{ algo }}</div>
+  </template>
 
-<script>
-export default {
+  <script>
+  import base from "@/mixins/base";
+  export default {
 	name: "App",
-
+	mixins: [base],
 	data() {
-		return{
-			text: "Hola Vue",
-		};
+	  return {
+		text: "Hola Vue",
+	  };
 	},
-	beforeCreate(){
-		console.log("beforeCreate", this.$data, this.$el);
+	beforeCreate() {
+	  console.log("beforeCreate", this.$data, this.$el);
 	},
-	created(){
-		console.log("create", this.$data, this.$el);
-	}, mounted(){
-		console.log("mounted", this.$data, this.$el);
-	}
+	created() {
+	  console.log("created", this.$data, this.$el);
+	},
+	mounted() {
+	  console.log("mounted", this.$data, this.$el);
+	},
+  };
+  </script>
 
-};
-</script>
-
-<style lang="scss">
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
-</style>
+  <style>
+  #app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+  }
+  </style>
