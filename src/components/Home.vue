7dffc0fa5a0f5806cbddb64198bfb4327a7034c0
Yormi Altamiranda <yormian@gmail.com>
2023-03-11 14:41:32 -0500

    :white_check_mark: update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,32 +4,23 @@
 	<button ref="btn">Click!</button>
   </template>
 
-  <script>
-  import { ref, toRefs, computed, watch, inject } from "vue";
-  export default {
-	props: {
-	  firstName: String,
-	  lastName: String,
-	},
-	setup(props, { expose }) {
-	  const { firstName, lastName } = toRefs(props);
-	  const fullName = computed(() => {
-		return `${firstName.value} ${lastName.value}`;
-	  });
-	  const username = inject("username");
-	  expose({
-		fullName,
-	  });
-	  const btn = ref(null);
-	  console.log(btn.value);
-	  watch(btn, (valor) => {
-		console.log(valor);
-	  });
-	  return {
-		fullName,
-		username,
-		btn,
-	  };
-	},
-  };
+  <script setup>
+  import { defineProps, defineExpose, ref, toRefs, computed, watch, inject } from "vue";
+  const props = defineProps({
+	firstName: String,
+	lastName: String,
+  });
+  const { firstName, lastName } = toRefs(props);
+  const fullName = computed(() => {
+	return `${firstName.value} ${lastName.value}`;
+  });
+  const username = inject("username");
+  defineExpose({
+	fullName,
+  });
+  const btn = ref(null);
+  console.log(btn.value);
+  watch(btn, (valor) => {
+	console.log(valor);
+  });
   </script>
